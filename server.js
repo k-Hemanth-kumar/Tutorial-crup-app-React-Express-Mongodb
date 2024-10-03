27a9287f2483deaadf440f5864968947e4f849db
Hemanth <hk0254862@gmail.com>
2024-10-03 19:36:47 +0530

    added db config

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express=require('express');
 const app=express();
+const mongoose=require('mongoose');
+const cors=require('cors');
+
+
+const corsOptions={
+    origin:"http://localhost:5001/"
+}
+app.use(cors(corsOptions));
+const config=require('./backend/config');
+console.log(config)
+mongoose.connect(`mongodb://${config.HOST}`)
 
 
 
